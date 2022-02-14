d9426ad7b23c1298a29b7c841a5cf59ddb4c2bfc
NelsonDiaz7 <nelsonfd34@gmail.com>
2022-02-13 19:58:27 -0500

    Class Model added

---
+++ src/co/edu/unbosque/model/Transaction.java
@@ -0,0 +1,88 @@
+package co.edu.unbosque.model;
+
+public class Transaction {
+
+    private String invoiceNumber;
+    private String stockCode;
+    private String description;
+    private String quantity;
+    private String invoiceDate;
+    private String unitPrice;
+    private String customerId;
+    private String country;
+
+    public Transaction(String invoiceNumber, String stockCode, String description, String quantity, String invoiceDate, String unitPrice, String customerId, String country) {
+        this.invoiceNumber = invoiceNumber;
+        this.stockCode = stockCode;
+        this.description = description;
+        this.quantity = quantity;
+        this.invoiceDate = invoiceDate;
+        this.unitPrice = unitPrice;
+        this.customerId = customerId;
+        this.country = country;
+    }
+
+    public String getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+    public String getStockCode() {
+        return stockCode;
+    }
+
+    public void setStockCode(String stockCode) {
+        this.stockCode = stockCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(String invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    public String getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(String unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
