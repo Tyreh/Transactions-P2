772567b25bcbdace038e907905d633ea07ce9093
Tyreh <oscar7u72@gmail.com>
2022-02-22 17:09:16 -0500

    Changed inheritance method.

--- src/co/edu/unbosque/view/DescriptionPanel.java
+++ src/co/edu/unbosque/view/DescriptionPanel.java
@@ -3,7 +3,7 @@ package co.edu.unbosque.view;
 public class DescriptionPanel extends TableManager {
 
     public DescriptionPanel() {
-        super(false, "Search", "DESCRIPTION_SEARCH", new String[]{"INVOICE NUMBER", "STOCK CODE", "DESCRIPTION", "QUANTITY", "INVOICE DATE", "UNIT PRICE", "CUSTOMER ID", "COUNTRY"}, new String[]{"Type a word to find: ", "Init month: ", "End month:"});
+        super(false, "Search", "DESCRIPTION_SEARCH", new String[]{"INVOICE NUMBER", "STOCK CODE", "DESCRIPTION", "QUANTITY", "INVOICE DATE", "UNIT PRICE", "CUSTOMER ID", "COUNTRY"}, new String[]{"Type a word to find: ", "Init month: ", "End month:"}, new String[]{"Order"});
         this.setVisible(false);
     }
 }
