878d2f9ddc22ec722d511f1fd49052248471f5de
Tyreh <oscar7u72@gmail.com>
2022-02-22 17:09:02 -0500

    Changed inheritance method.

--- src/co/edu/unbosque/view/AveragePanel.java
+++ src/co/edu/unbosque/view/AveragePanel.java
@@ -3,9 +3,9 @@ package co.edu.unbosque.view;
 public class AveragePanel extends TableManager {
 
     public AveragePanel() {
-        super(false, "Search", "AVERAGE_SEACH",
+        super(false, "Search", (Object) "AVERAGE_SEACH",
                 new String[]{"ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"},
-                new String[]{"Group by country"}, new String[]{"COUNTRY_CHECKBOX"});
+                new String[]{"Group by country"});
         this.setVisible(false);
     }
 }
