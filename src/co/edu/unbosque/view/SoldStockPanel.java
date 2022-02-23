74ceb0e12446d14c410f4de3b721d9959f7ee583
NelsonDiaz7 <nelsonfd34@gmail.com>
2022-02-22 19:18:57 -0500

    SoldStockPanel.java full documentation

--- src/co/edu/unbosque/view/SoldStockPanel.java
+++ src/co/edu/unbosque/view/SoldStockPanel.java
@@ -1,7 +1,14 @@
 package co.edu.unbosque.view;
 
+/**
+ * Clase extendida de TableManager que permite cambiar el header y funciones de la tabla principal
+ */
 public class SoldStockPanel extends TableManager {
 
+
+    /**
+     * Metodo que permite cambiar los valores del header por medio de parametros adecuados a panel de cantidad de ventas
+     */
     public SoldStockPanel() {
         super(true, "Search", "STOCK_SOLD_SEARCH", new String[]{"INVOICE NUMBER", "STOCK CODE", "DESCRIPTION", "QUANTITY", "INVOICE DATE", "UNIT PRICE", "CUSTOMER ID", "COUNTRY"}, new String[]{"Type a stock code to find:"});
         this.setVisible(false);
