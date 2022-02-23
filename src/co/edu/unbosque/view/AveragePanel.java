c0ed83d1bf4ce9c534ba322ba80cb5e971579838
NelsonDiaz7 <nelsonfd34@gmail.com>
2022-02-22 19:06:46 -0500

    AveragePanel.java, DescriptionPanel.java and InvoicePanel.java full documentation

--- src/co/edu/unbosque/view/AveragePanel.java
+++ src/co/edu/unbosque/view/AveragePanel.java
@@ -1,7 +1,13 @@
 package co.edu.unbosque.view;
 
+/**
+ * Clase extendida de TableManager que permite cambiar el header y funciones de la tabla principal
+ */
 public class AveragePanel extends TableManager {
 
+    /**
+     * Metodo que permite cambiar los valores del header por medio de parametros adecuados a panel de promedio
+     */
     public AveragePanel() {
         super(false, "Search", (Object) "AVERAGE_SEACH",
                 new String[]{"ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"},
--- src/co/edu/unbosque/view/DescriptionPanel.java
+++ src/co/edu/unbosque/view/DescriptionPanel.java
@@ -1,7 +1,13 @@
 package co.edu.unbosque.view;
 
+/**
+ * lase extendida de TableManager que permite cambiar el header y funciones de la tabla principal
+ */
 public class DescriptionPanel extends TableManager {
 
+    /**
+     * Metodo que permite cambiar los valores del header por medio de parametros adecuados a panel de descripcion
+     */
     public DescriptionPanel() {
         super(false, "Search", "DESCRIPTION_SEARCH", new String[]{"INVOICE NUMBER", "STOCK CODE", "DESCRIPTION", "QUANTITY", "INVOICE DATE", "UNIT PRICE", "CUSTOMER ID", "COUNTRY"}, new String[]{"Type a word to find: ", "Init month: ", "End month:"}, new String[]{"Order"});
         this.setVisible(false);
