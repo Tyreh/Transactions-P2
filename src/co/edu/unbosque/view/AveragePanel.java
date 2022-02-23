8cc39f7b5b017c2d921e37d4318a9ca5de1e5da2
Tyreh <oscar7u72@gmail.com>
2022-02-23 12:43:27 -0500

    Added new column in table for countries.

--- src/co/edu/unbosque/view/AveragePanel.java
+++ src/co/edu/unbosque/view/AveragePanel.java
@@ -9,9 +9,11 @@ public class AveragePanel extends TableManager {
      * Metodo que permite cambiar los valores del header por medio de parametros adecuados a panel de promedio
      */
     public AveragePanel() {
-        super(false, "Search", (Object) "AVERAGE_SEACH",
-                new String[]{"ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"},
+        super(false, "Search", (Object) "AVERAGE_SEARCH",
+                new String[]{"COUNTRY","JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"},
                 new String[]{"Group by country"});
         this.setVisible(false);
     }
+
+
 }
